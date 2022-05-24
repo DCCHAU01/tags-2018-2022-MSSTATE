2e058a8e6eae86692e4b1027a1f333797ff9842b
DChivalri <83895886+DChivalri@users.noreply.github.com>
2022-05-24 10:48:41 -0500

    Create A1-BasicASCIIArt.py

---
+++ Intro-Comp-Prog-Python-Fall-2018/A1-BasicASCIIArt.py
@@ -0,0 +1,34 @@
+#Date Due: 8/27/18
+#Program Description: ASCII Art
+
+#My Initials
+print("l---    ---\ ")
+print("l    \ /     ")
+print("l    l l     ")
+print("l    l l     ")
+print("l    / \     ")
+print("l---    ---/ ")
+
+print("=======================================================================")
+#Wanna Be Mountains
+print("           /    ")#1
+print("          /-\   ")#2
+print("         /   \  ")#3
+print("      /\/     \/")#4
+print("  /\ /--\     / ")#5
+print(" /--\/  \    /  ")#6
+print("/    /  \   /   ")#7
+print("    /    \ /    ")#8
+print("   /      \     ")#9
+print("  /        \    ")#10
+
+print("=======================================================================")
+#An Anonymous Creature
+print(" ^      ^ ")#1
+print("( )    ( )")#2
+print("(@)^^^^(@)")#3
+print(" (      ) ")#4
+print("( O    O )")#5
+print(" ( -  - ) ")#6
+print("  (\__/)  ")#7
+print("   (  )   ")#8
