d33b8a28baac5ed5354587898efb5e4f10bf1269
DChivalri <83895886+DChivalri@users.noreply.github.com>
2022-05-24 11:13:06 -0500

    Create A1-HelloWorld.cpp

---
+++ Intermediate-Comp-Prog-C++-Spring-2019/A1-HelloWorld.cpp
@@ -0,0 +1,14 @@
+/*
+	Program Description:
+		example of "Hello World" in C++.
+*/
+#include <iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+	cout << "Hello World! I am in CSE ____ and this class is awesome \n";
+	cout << "I laze around\n";
+	cout << "I find my neighbors either reserved or elbow-invasive\n";
+
+}
