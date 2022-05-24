677cf2e6f25200c2bf7e644d87ad6b6dbceaf848
DChivalri <83895886+DChivalri@users.noreply.github.com>
2022-05-24 11:14:34 -0500

    Create A2-DebuggingPart1.cpp

---
+++ Intermediate-Comp-Prog-C++-Spring-2019/A2-DebuggingPart1.cpp
@@ -0,0 +1,32 @@
+/*
+	Program Description:
+		Debugging a small program part 1.
+*/
+#include <iostream> 
+
+using std::cout; 
+using std::endl;
+
+int main() 
+{
+	int x = 10; 
+	int y = 10;
+	
+	int z, a, b, c;
+
+	z = x + y;
+
+	cout << "z is " << z << endl;
+
+	a = x - y;
+
+	cout << "a is " << a << endl;
+
+	b = x * y;
+
+	cout << "b is " << b << endl;
+
+	c = x / y;
+
+	cout << "c is " << c << endl;
+}
